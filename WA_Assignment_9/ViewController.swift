a380e8b55816e60aae2bf79a55f643f43930731e
Blashko Maksym <Blashkoma@gmail.com>
2021-02-27 00:12:33 +0200

    Initial Commit

---
+++ WA_Assignment_9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WA_Assignment_9
+//
+//  Created by Blashko Maksim on 27.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
